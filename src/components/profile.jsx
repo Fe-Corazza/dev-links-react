87323e1dcda6a574f1fb54f59003317d554b4686
Fe-Corazza <felipecorazza09@gmail.com>
2023-08-10 17:12:09 -0300

    finalizando aula 10/08

---
+++ src/components/profile.jsx
@@ -0,0 +1,9 @@
+export default function profile() {
+    return(
+        <div className="profile">
+            <img src="" alt="" />
+            
+        </div>
+    )
+    
+};
---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Profile from "../components/profile";
+
+export default function app() {
+    return(
+    <>
+   <Profile></Profile>
+        </>
+    );
+};
